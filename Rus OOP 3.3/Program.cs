df8f24faae50043ce80c3e61784b2539716c8a1c
Ruslan <ruslanlogin78@gmail.com>
2020-05-20 23:27:10 +0300

    9th

---
+++ Rus OOP 3.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Rus_OOP_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader fromFile = new StreamReader("D:\\input.txt");
+            string sentence = fromFile.ReadLine();
+            fromFile.Close();
+            Console.WriteLine(sentence);
+            char[] c = sentence.ToCharArray();
+            int n = 0;
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (Char.IsNumber(c[i]))
+                {
+                    n++;
+                }
+            }
+            Console.WriteLine("кількість чисел у тексті {0}",n);
+            File.WriteAllText("D:\\output.txt", string.Join("кількість чисел у тексті {0} ", n));
+
+        }
+    }
+}
